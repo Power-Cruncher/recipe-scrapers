91bc13e2486e2545e2ed89e7889ad201fdd1541d
Power-Cruncher <RegulusShoe@gmail.com>
2026-07-03 16:39:30 +1200

    Removed  uneeded functions ingredients(self) and _clean_ingredient_text(self, li):

--- recipe_scrapers/simplehomeedit.py
+++ recipe_scrapers/simplehomeedit.py
@@ -7,31 +7,6 @@ class SimpleHomeEdit(AbstractScraper):
     def host(cls):
         return "simplehomeedit.com"
 
-    def _clean_ingredient_text(self, li):
-        # Remove the checkbox container - this site's theme renders the
-        # "▢" checkbox glyph as real screen-reader text content (most WPRM
-        # themes do this via CSS ::before instead, which never shows up in
-        # get_text()), so it has to be stripped explicitly here.
-        checkbox = li.select_one("span.wprm-checkbox-container")
-        if checkbox:
-            checkbox.extract()
-
-        return li.get_text(" ", strip=True)
-
-    def ingredients(self):
-        ingredients = []
-        for li in self.soup.select(
-            "div.wprm-recipe-ingredients-container li.wprm-recipe-ingredient"
-        ):
-            text = self._clean_ingredient_text(li)
-            if text:
-                ingredients.append(text)
-
-        if not ingredients:
-            raise ElementNotFoundInHtml("Could not find ingredients.")
-
-        return ingredients
-
     def instructions(self):
         instructions = []
         for li in self.soup.select(
